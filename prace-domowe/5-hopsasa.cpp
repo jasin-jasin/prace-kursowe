ab825b303145b70018672f2710323e6eba301fec
Karol Jasinski <karolyasinski@gmail.com>
2022-11-15 23:12:37 +0100

    co prawda nie jest to dokładnie hopsasa, natomiast idea jest spełniona

---
+++ prace-domowe/5-hopsasa.cpp
@@ -0,0 +1,38 @@
+// 5. Wypisz liczby całkowite od 1 do 100, ale:
+// - Zamiast podzielnych przez 3 wypisz "Hopsasa"
+// - Zamiast podzielnych przez 5 wypisz "Tralala"
+// - Zamiast podzielnych przez 3 i przez 5 wypisz "HopsasaTralala" (to również wynika z powyższych zasad).
+
+#include <iostream>
+
+int main () {
+    int x = 0;
+    int y = 0;
+    int a = 0;
+    int b = 0;
+    std::cout << "Podaj od jakiej liczby chcesz sprawdzać podzielność: ";
+    std::cin >> x;
+    std::cout << "Podaj do jakiej liczby chcesz sprawdzać podzielność: ";
+    std::cin >> y;
+    std::cout << "Podaj przez jaką pierwszą liczbę chcesz sprawdzać podzielność: ";
+    std::cin >> a;
+    std::cout << "Podaj przez jaką drugą liczbę chcesz sprawdzać podzielność: ";
+    std::cin >> b;
+    
+    while (x <= y)
+     if (x % a == 0 && x % b == 0)  // tu będą warunki do fizz buzz
+    {std::cout << "Liczba " << x << " jest podzielna przez " << a <<  " i przez " << b << "\n";
+    x = x + 1;    
+    }
+    else if (x % a == 0 && x % b != 0)
+    {std::cout << "Liczba " << x << " jest podzielna przez " << a <<  " nie jest podzielna przez " << b << "\n";
+    x = x + 1;    
+    }
+    else if (x % a != 0 && x % b == 0)
+    {
+            std::cout << "Liczba " << x << " nie jest podzielna przez " << a <<  " i jest podzielna przez " << b << "\n";
+    x = x + 1 ;}
+        
+        else {std::cout << "Liczba " << x << " nie jest podzielna ani przez " << a <<  " ani przez " << b << "\n";
+    x = x + 1;} 
+}

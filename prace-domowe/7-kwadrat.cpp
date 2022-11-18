8d8eb45f97be2deecd547e418fc52206f0eccc77
Karol Jasinski <karolyasinski@gmail.com>
2022-11-18 18:45:54 +0100

    jeszcze wersja nie do końca działająca

---
+++ prace-domowe/7-kwadrat.cpp
@@ -0,0 +1,57 @@
+// 7. Przyjmij liczbę N, a następnie wypisz na ekran kwadrat złożony z // zer i jedynek ułożonych tak, jak czarne i białe pola na szachownicy.
+// Dla N = 4 wynikiem powinno być:
+//
+// 0101
+// 1010
+// 0101
+// 1010
+//
+//
+
+#include <iostream>
+
+int main () {
+    int n = 0;
+    int licznik_wierszy = 1;
+    int licznik_wewn_1 = 1;
+    int licznik_wewn_2 = 1;
+    std::cout << "Podaj o jakim boku zrobić kwadrat: ";
+    std::cin >> n;
+
+    for ( ; licznik_wierszy <=n; licznik_wierszy += 1)
+    {
+        if (licznik_wierszy % 2 !=0)
+        {
+            while (licznik_wewn_1 <= n)  // rysuje wiersze nieparzyste
+            {
+            if (licznik_wewn_1 % 2 == 0)
+            {
+            std::cout << "1";
+            }
+        else
+            {
+            std::cout << "0";
+            }
+        licznik_wewn_1 = licznik_wewn_1 + 1;    // licznik += 1;
+            }
+        std::cout << "\n";
+        }
+        else {
+
+            while (licznik_wewn_2 <= n)  // rysuje wiersze parszyste
+            {
+                if (licznik_wewn_2 % 2 != 0)
+                {
+                std::cout << "1";
+                }
+                else
+                {
+                std::cout << "0";
+                }
+        licznik_wewn_2 = licznik_wewn_2 + 1;    // licznik += 1;
+        }
+        std::cout << "\n";
+        }
+    std::cout << "Licznik wierszy wynosi: " << licznik_wierszy;
+    }
+}

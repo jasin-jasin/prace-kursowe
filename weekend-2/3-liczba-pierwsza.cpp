b7531af15de1493decabd2afe1630111513584a8
Karol Jasinski <karolyasinski@gmail.com>
2022-11-19 17:12:48 +0100

    dupa

--- weekend-2/3-liczba-pierwsza.cpp
+++ weekend-2/3-liczba-pierwsza.cpp
@@ -5,7 +5,17 @@
     
 bool czy_pierwsza (int x) {
     bool wynik = true;
-    if (x == 1) return false;
+    if (x <= 1) return false;
+
+// RÓŻNE MOŻLIWOŚCI OPTYMALIZACJI
+// if (n == 2) 
+//    return true;
+// if (n % 2) == 0 => wywalamy sprawdzanie parzystych wtedy for (int i = 3; i <n; i +=2)
+//     return false; 
+// jeśli pierwiastek kwadratowy z x  <= n (n <= sqrt (x) => zmieniamy w liczniku pętli
+// lepiej ten warunek zapisać n * n <= x
+// porównaj czas wykonywania programu w kolejnych krokach optymalizacji time ./a.out
+    
     for (int n = 2; n <= x /2; n += 1) { // alternatywny zapis (;; i +=1) oznacza że zwiększaj licznik i o 1; i+=2 - zwiększaj o 2; i +=-1 zmniejszaj o 1)
         if (x % n == 0)
         {wynik = false;

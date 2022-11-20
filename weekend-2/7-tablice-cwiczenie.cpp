5c87e24941658bc56a41887d04678a822bc3f5bc
Karol Jasinski <karolyasinski@gmail.com>
2022-11-20 12:21:39 +0100

    poprawki żeby ładnie wyglądało

--- weekend-2/7-tablice-cwiczenie.cpp
+++ weekend-2/7-tablice-cwiczenie.cpp
@@ -4,15 +4,16 @@
 # include <iostream>
 
 int main () {
-    int tab [10]; 
-    for  (int i = 9; i >=0; i -= 1) 
+    const int rozmiar = 10;
+    int tab [rozmiar]; 
+    for  (int i = 0; i < rozmiar; i += 1) 
     {
-        std::cout << "Podaj " << i << " liczbę: " << "\n";
-        std::cin >> tab[i];   // robimy pętle, i wpisujemy kolejne liczby do tablicy począwszy od tab[9]
+        std::cout << "Podaj " << i+1 << " liczbę: " << "\n";
+        std::cin >> tab[i];   // robimy pętle, i wpisujemy kolejne liczby do tablicy począwszy od tab[0]
     }
-    for  (int i = 0; i <10; i += 1)
+    for  (int i = rozmiar - 1; i >= 0; i -= 1)
     {
-        std::cout << "tab[" << i << "] = " << tab[i] << "\n"; // pętla przechodząca po polach tablicy i wypisujące kolejne wartości, ale już w normalnej kolejności
+        std::cout << "tab[" << i << "] = " << tab[i] << "\n"; // pętla przechodząca po polach tablicy i wypisujące kolejne wartości, ale już w odwrotnej kolejności
     }
     std::cout << "\n";
 }

aa84acfdbb0eb1a1a3d615e8af503ed3a1ae2bbf
Karol Jasinski <karolyasinski@gmail.com>
2022-11-20 11:16:35 +0100

    funkcja rozwiązujące rozwiązanie liniowe

---
+++ weekend-2/6-rownanie-liniowe.cpp
@@ -0,0 +1,48 @@
+// funkcja ma zwrócić ile jest rozwiązań równania liniowa
+// jeśli jest rozwiązanie ,to wpisujemy je pod zadany adres
+// 0 = ax + b (dla nieskończenie wielu albo braku zwróć -1 
+// W przypadku braku rozwiązań zwróć 0
+// W pozostałych przypadkach zwróć 1
+// jeśli istnieje dokładnie 1 rozwiązanie to wpisz je pod adres rozwiazanie
+
+#include <iostream>
+    
+int rownanie_liniowe (double a,  double b, double* rozwiazanie)
+    { 
+    if (a == 0.0 && b == 0.0)
+        return -1;
+    if (a == 0.0 && b != 0.0)
+        return 0;
+    else {
+        *rozwiazanie = -b/a; // dereferencja
+        return 1;
+    }
+}
+
+// alternatywny zapis warunku: 
+// if (a != 0.0) {return 1} (bo wtedy 1 rozwiązanie)
+// else if  (b !=0.0) {return 0} (bo wtedy wiemy  że a jest 0)
+// else  return -1)
+
+    
+int main () {
+    double a = 0.0;
+    double b = 0.0;
+    double rozwiazanie = 0.0;
+    int ile = 0;
+    std::cout << "Podaj liczbę a: ";
+    std::cin >> a;
+    std::cout << "Podaj liczbę b: ";
+    std::cin >> b;   
+    ile = rownanie_liniowe (a, b, &rozwiazanie);
+    if (ile == -1)
+    {
+     std::cout << "Równanie liniowe ma nieskończenie wiele rozwiązań" ;
+    }
+    if (ile == 0)
+    {
+    std::cout << "Równanie liniowe nie ma rozwiązań" ;
+    }
+    if (ile == 1)
+    std::cout << "Liczba równania liniowego 0 = ax +b wynosi: " << ile << " i wynosi ono " << rozwiazanie << "\n";
+}

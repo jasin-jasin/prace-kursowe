7560c5f591613eb08dfeca062363ea58b7ac5734
Karol Jasinski <karolyasinski@gmail.com>
2022-11-29 23:23:38 +0100

    średnia za pomocą funkcji

--- prace-domowe-2/03-srednia-elementow-tablicy.cpp
+++ prace-domowe-2/03-srednia-elementow-tablicy.cpp
@@ -1,27 +1,56 @@
 // 3. Napisz funkcję srednia(), która przyjmie jako argumenty tablicę liczb całkowitych i jej rozmiar, po czym zwróci średnią arytmetyczną elementów w tej tablicy
 
+
+// rozwiązanie koszerne - z funkcją
+
 #include <iostream>
 
-// double srednia (cos tam, cos tam) {
-//    double srednia = 0;
-//    return srednia;
-//    }
-// }
-
+double srednia (int* tab, int n) 
+    {
+    double srednia = 0;
+    double suma = 0;
+    for (int i = 0; i < n; i += 1) // przechodzimy po tablicy która ma n elementów
+        {
+           suma += tab[i];
+        }    
+    srednia = suma / n;
+    return srednia;
+    }
 
 int main () {
-    const int rozmiar = 10;
+    const int rozmiar = 5;
     int tab [rozmiar]; 
-    double suma = 0;
-    double srednia = 0;
     for  (int i = 0; i < rozmiar; i += 1) 
     {
         std::cout << "Podaj " << i+ 1 << " liczbę: " << "\n";
-        std::cin >> tab[i];   // robimy pętle, i wpisujemy kolejne liczby do tablicy począwszy od tab[0]
-	suma += tab[i];
-	std::cout << "Suma: " << suma << "\n"; 
+        std::cin >> tab[i];   // robimy pętle, i przyjmujemy kolejne liczby do tablicy począwszy od tab[0]
     }
-    srednia = suma / rozmiar;
-    std::cout << "Średnia z elementów tablicy wynosi: " << srednia << "\n"; 
-}
- 
+    std::cout << "Średnia elementów w tabilcy to: " << srednia (tab, rozmiar) << "\n"; // tab - to jest przekazanie adresu tablicy
+}  
+  
+
+
+
+
+
+
+// == rozwiązanie chu.e - bez funkcji ===
+// 
+// #include <iostream>
+// 
+// int main () {
+//     const int rozmiar = 10;
+//     int tab [rozmiar]; 
+//     double suma = 0;
+//     double srednia = 0;
+//     for  (int i = 0; i < rozmiar; i += 1) 
+//     {
+//         std::cout << "Podaj " << i+ 1 << " liczbę: " << "\n";
+//         std::cin >> tab[i];   // robimy pętle, i wpisujemy kolejne liczby do tablicy począwszy od tab[0]
+// 	suma += tab[i];
+// 	std::cout << "Suma: " << suma << "\n"; 
+//     }
+//     srednia = suma / rozmiar;
+//     std::cout << "Średnia z elementów tablicy wynosi: " << srednia << "\n"; 
+// }
+//  

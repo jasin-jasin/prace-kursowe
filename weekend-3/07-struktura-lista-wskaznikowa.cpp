ab357a2cbc1b832b4f22e5d6e4638922595f5bf7
Karol Jasinski <karolyasinski@gmail.com>
2022-12-03 15:48:00 +0100

    zwalnianie pamięc zaalokowanej na listę

--- weekend-3/07-struktura-lista-wskaznikowa.cpp
+++ weekend-3/07-struktura-lista-wskaznikowa.cpp
@@ -12,6 +12,7 @@ struct element {
 
 // wstawienie nowego elementu na początek listy
 // funkcja zwraca adres nowego elementu
+
 element* wstaw (element* lista, int nowa_wartosc) {
     element* tmp = new element; // tworzymy nowy element, tmp jest ADRESEM elementus
     (*tmp).wartosc = nowa_wartosc; // wyłuskanie ze struktury, deferencja powoduje, że wyciągamy CAŁĄ strukturę, a ze struktury chcemy pole wartość
@@ -29,16 +30,28 @@ void wypisz (element* lista) {
     std::cout << '\n';
 }
 
+// w tym programie brakuje jeszcze zwolnienia pamięci, bo alokujemy, ale nie zwalniamy.
+
+void zwolnij (element* lista) {
+    
+    while (lista != nullptr)
+    {
+    element* tmp = lista;   // tworzymy wskaźnik tmp, który wskazuje na listę
+    lista = lista->nastepny; // przestawiamy wskaźnik na kolejny - szykujemy się na kolejny krok
+    delete tmp;  // kasujemy adres pamięci tmp
+    }            // po wyjściu z while'a nie trzeba nic zwalniać bo jesteśmy na nullptr;
+}
 
 int main () {
     element* lista = nullptr;        // wskaźnik pokazujący na null pointer, lista jest pusta
     lista = wstaw (lista, 5);
     lista = wstaw (lista, 3);
     lista = wstaw (lista, 6);
-    for (int i =0; i < 1000000; i +=1){
+    for (int i =0; i < 100; i +=1){
         lista = wstaw(lista, i);
     }             // zrobiliśmy listę, ktora może przyjąć dowolną duż aliczbę elementów
     
     wypisz(lista);
                                     // za pomocą operatora new tworzymy nowe obiekty
+    zwolnij(lista);         // wywołujemy funckję zwalniającą pamięć
 }

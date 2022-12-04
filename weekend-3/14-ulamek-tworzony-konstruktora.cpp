8f49d03e1dba89c56e2b0d983fd300ce226d9c38
Karol Jasinski <karolyasinski@gmail.com>
2022-12-04 12:35:00 +0100

    tworzenie ulamkow za pomocą konstruktora

---
+++ weekend-3/14-ulamek-tworzony-konstruktora.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct ulamek {
+    int licznik;
+    int mianownik;      
+    void wypisz_ulamek () const {
+    std::cout << "(" << licznik << "/" << mianownik << ")" << '\n';
+    }
+    ulamek (int a, int b) 
+    {
+    licznik = a;
+    mianownik = b;
+    }
+    ulamek ulamek_mnoz(const ulamek& b) const { 
+        ulamek c {licznik * b.licznik, mianownik * b.mianownik};
+        return c;
+    }
+};
+
+int main() {
+       ulamek a{2,0};
+       ulamek b{3,5};
+    a.wypisz_ulamek ();
+    b.wypisz_ulamek ();
+//    ulamek_mnoz(a,b).wypisz_ulamek ();
+        ulamek c = a.ulamek_mnoz (b);
+        c.wypisz_ulamek();
+}

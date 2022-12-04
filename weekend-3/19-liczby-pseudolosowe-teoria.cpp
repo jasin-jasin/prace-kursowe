16fd3c47ae02b35219be3c6ee3c64f61babfdfd1
Karol Jasinski <karolyasinski@gmail.com>
2022-12-04 17:09:33 +0100

    poprawki do pseudolosowych

--- weekend-3/19-liczby-pseudolosowe-teoria.cpp
+++ weekend-3/19-liczby-pseudolosowe-teoria.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <cstlib>   // standardowa biblioteka języka C, std::rand (), std:srand ()
+#include <cstdlib>   // standardowa biblioteka języka C, std::rand (), std:srand ()
 #include <ctime>    // std::time()
 
 
 int main () {
-    std::srand ();          // ziarno genertara liczb pseudolosowych
+    std::srand (20);          // ziarno genertara liczb pseudolosowych
     std::srand (std::time(nullptr)); // ustawiamy ziarno jako caas od 1.1.1970
-    std::cout << std::time(nullptr) << 'n';  // std time zwraca liczbę sekund od od 1 stycznia 1970 od pólnocy 
-    int x = std::rand();  // to powinnno przypisać do x jakąś losową liczbę
+    std::cout << std::time(nullptr) << '\n';  // std time zwraca liczbę sekund od od 1 stycznia 1970 od pólnocy 
+    int x = std::rand() % 10;  // to powinnno przypisać do x jakąś losową liczbę
     std::cout << x << '\n';
     std::cout << std::rand() << '\n';
     std::cout << std::rand() << '\n';
+    x = std::rand () % 6 + 1;
+    std::cout << x << '\n';
 }

113e26b37b1ecd94821c20d58afaa6ae74098352
kurs <oszerg@gmail.com>
2023-01-28 14:37:38 +0100

    poprawki do działania na plikach

--- weekend-5/6-wypisaywanie-klas.cpp
+++ weekend-5/6-wypisaywanie-klas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 class Ulamek {
 public:
@@ -8,11 +9,11 @@ public:
     int mianownik;
 };
 
-void operator<<(std::ostream& os, const Ulamek& u) {
+std::ostream& operator<<(std::ostream& os, const Ulamek& u) {
                 // std::ostream nie można kopiować, dlatego przekazujemy przez referencję
     os << u.licznik << '/' << u.mianownik;
                 // ponieważ licznik i mianownik są klasą prywatną, a operator nie jest metodą ułamka, to nie ma dostępu do pól prywatnych
-    
+    return os;
 }
 
 
@@ -23,5 +24,9 @@ int main () {
                             // bierzemy jedynkę w poniższym przykładzie i przesuwamy o 20 pozycji w lewo
     std::cout << 1 << 20 << '\n'; //=> to wypisze normalnie 120
     std::cout << (1 << 20) << '\n'; // => NAWIASY mają znaczenie, najpierw wykona się działanie w nawiach
+    std::cout << a << '\n'; // tu będzie błąd jeśli funkcja zwraca void więc lewa część będzie wyglądała void << '\n' (a to jest char)
+                            // ale jeśli funkcja zwraca referencję na ostream, to już będzie ok.
+    std::ofstream plik{"wyjscie.txt"}; // output file stream; ten obiket w momencie tworzenia stworzy plik.txt i będzie mógł do niego zapisywać.
+    plik << "Ala ma kota\n" << "ulamek a = " << a << '\n';
     
 }
